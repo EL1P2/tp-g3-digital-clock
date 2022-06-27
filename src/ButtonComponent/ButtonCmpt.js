8c0b314dbd1f1143559d6e6d84fe2aa7dd5fbb7b
Herizoran <herizo.ran@gmail.com>
2022-06-27 21:52:24 +0300

    added react-router module

--- src/ButtonComponent/ButtonCmpt.js
+++ src/ButtonComponent/ButtonCmpt.js
@@ -4,7 +4,7 @@ export const BtnComponent = (props) => {
     let btn_component, class_name, exec_func;
     btn_component  = props.btn_component;
     class_name  = props.class_name;
-    exec_func = props.exec_func
+    exec_func = props.exec_func;
     return (
         <button id="boutton" className={class_name} onClick={exec_func}>
             {btn_component}
