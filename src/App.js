c6b6fbbe2aa48c64b4ddce05c6ef8b5cfcc07dd3
Herizoran <herizo.ran@gmail.com>
2022-06-27 22:04:45 +0300

    updated App.js from router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Clock from './Clock';
 
-
-
 function App() {
-  return <Clock></Clock>;
+  return (
+    <div className="App">
+      <header>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path='*' element={<Clock />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </header>
+    </div>
+    );
 }
 
 export default App;
